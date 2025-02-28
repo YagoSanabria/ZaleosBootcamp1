9cad3b56a230791f83517a7e5f520bb718bac3d4
Tian3030 <timsalomf@gmail.com>
2025-02-28 12:01:43 +0100

    Send & Recv as functions

--- cliente.sh
+++ cliente.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
-#Escritura al servidor
-echo -e "$1" | nc -w 0 -u localhost 8080
-echo "$(nc -u -l 8080 | head -n 1)" > /tmp/APIwebpage.html
+#CLIENTE->SERVER: 1 LINEA: CONTENTS+CKSUM separado por columnas
+function send(){	#$1 sendInfo
+	RESPONSE=$1
+    CKSUM=$(echo "$RESPONSE" | cksum)
 
-firefox /tmp/APIwebpage.html &
+	SENDING="$RESPONSE $CKSUM"
+	echo "Sending: $SENDING"
+	echo "$SENDING"|nc -w 0 -u localhost 8080
+}
+
+
+#SERVER->CLIENTE: 1 LINEAS CONTENTS+CKSUM
+#Escritura al servidor
+WEBPATH="/tmp/APIwebpage.html"
+recv(){
+	RESPONSE=$(nc -u -l 8080 | head -n 1)
+	echo "response is $RESPONSE"
+	REVCKSUM=$(echo "$RESPONSE" | rev | awk '{print $2}')
+	echo "revchecksum $REVCKSUM"
+
+	RESPONSE=$(echo ${RESPONSE} | sed 's/ [0-9]* [0-9]*$//')
+	echo "RESPONSE $RESPONSE"  > $WEBPATH
+	echo "REV: $REVCKSUM"
+}
+
+#check correct usage
+if [ $# -eq 0 ]
+then
+
+	echo -e "Error: Incorrect usage of client.sh\nUse: $0 <param>"
+	exit 1
+fi
+
+send $1
+
+recv
+
+echo "Exited successfully"
+firefox  $WEBPATH &
 exit 0
 
 
--- servidor.sh
+++ servidor.sh
@@ -10,27 +10,49 @@ function cumpleRegex(){
 	fi
 }
 
+#SERVER->CLIENTE: 1 LINEAS: CONTENTS+CKSUM
+send(){	#$1 sendInfo
+	RESPONSE=$1
+	FINALRESPONSE=$(echo "$RESPONSE $(echo $RESPONSE | cksum)")
+	echo "$FINALRESPONSE" |nc -w 0 -u localhost 8080
+	echo "Sending: $FINALRESPONSE"
+
+}
+
+
+#CLIENTE->SERVER: 1 LINEAS: CONTENTS+CKSUM separados por columnas
+QUERY=""
+recv(){
+	QUERY=""
+	QUERY=$(nc -u -l 8080 | head -n 1)
+	URI=$(echo $QUERY | awk '{print $1}')
+	CKSUM=$(echo $QUERY | awk '{print $2}')
+	ACTUALCKSUM=$(echo $URI | cksum | awk '{print $1}')
+
+	echo "Parameters: QUERY: $QUERY,URI: $URI,CKSUM: $CKSUM,ACTUALCKSUM:$ACTUALCKSUM"
+
+	if [ $CKSUM = $ACTUALCKSUM ]
+	then
+		echo "GET REQUEST ACCEPTED TO $URI"
+		QUERY=$URI
+	fi
+}
+
+
 while [ 1 ] 
 do
-QUERY=$(nc -u -l 8080 | head -n 1)
+recv
 echo "GET request for $QUERY"
 
-	if [ $QUERY = "/index" ]
+	if [ "$QUERY" = "/index" ]
 	then
 		RESPONSE=$(cat web/index.html | tr -d '\n')
-		echo $RESPONSE | nc -w 0 -u localhost 8080 
-	elif [[ $QUERY =~ ^/regex/.*/.*$ ]]
-	then
-		PALABRA=echo $QUERY | #echo $IN | tr ";" "\n" 
-
-		REGEX=
-		cumpleRegex()
+		send "$RESPONSE"
 	else
 		RESPONSE=$(cat web/errpage.html | tr -d '\n')
-		echo $RESPONSE | nc -w 0 -u localhost 8080 
+		send "$RESPONSE"
 	fi
-
-
+	echo "QUERY RESPONDED"
 done
 
 
