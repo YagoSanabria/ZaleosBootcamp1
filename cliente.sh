3e2dc8216d45ae2147f8375c7a400d7fe8c6f8e6
YagoSanabria <yago.sanabria.gavin@gmail.com>
2025-03-04 09:46:55 +0100

    client prefect

--- cliente.sh
+++ cliente.sh
@@ -2,24 +2,22 @@
 
 PORT=8080
 
-#CLIENTE->SERVER: 1 LINEA: CONTENTS+CKSUM separado por columnas
-function send(){	#$1 sendInfo
+#CLIENT->SERVER: 1 LINE: CONTENTS+CKSUM divided by columns
+function send(){	#$1 arg = sendInfo
 	RESPONSE=$1
     CKSUM=$(echo "$RESPONSE" | cksum)
-
 	SENDING="$RESPONSE $CKSUM"
 	echo "Sending: $SENDING"
 	echo "$SENDING"|nc -w 0 -u localhost $PORT
 }
 
-
-#SERVER->CLIENTE: 1 LINEAS CONTENTS+CKSUM
-#Escritura al servidor
-WEBPATH="/tmp/APIwebpage.html"
+#SERVER->CLIENT: 1 LINE CONTENTS+CKSUM
+WEBPATH="/tmp/APIwebpage.html" #temporal local file path
 recv(){
 	RESPONSE=$(nc -u -l $PORT | head -n 1)
 	echo "response is $RESPONSE"
 	REVCKSUM=$(echo "$RESPONSE" | rev | awk '{print $2}')
+	#analize checksum reversed to get last bytes of msg ignoring msg length
 	echo "REVCK: $REVCKSUM"
 	RESPONSE=$(echo ${RESPONSE} | sed 's/ [0-9]* [0-9]*$//')
 	CKSUM=$(echo ${RESPONSE} | cksum | awk '{print $1}' | rev)
@@ -37,14 +35,14 @@ recv(){
 #check correct usage
 if [ $# -gt 0 ]
 then
-
 	echo -e "Error: Incorrect usage of client.sh\nUse: $0 and type the URL as input later"
 	exit 1
 fi
 
+#client loop
 while [ 1 ]
 do
-	read -p "Enter a QUERY: " URI
+	read -p "Enter a QUERY: ("exit" to finish)" URI
 	if [ $URI = "exit" ]
 	then
 		break
@@ -54,9 +52,6 @@ do
 	firefox  $WEBPATH &
 
 done
-
+#exit
 echo "[+] Exited successfully"
 exit 0
-
-
-
